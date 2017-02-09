2e55f544f2d6d169deab3eca52ae26e7d007d18d
bodiddlie <bodiddlie@gmail.com>
2017-02-09 15:57:14 -0700

    changed some styling stuff

--- src/header.jsx
+++ src/header.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 
 import {auth} from './firebase';
 
-export const Header = ({user}) => (
+export const Header = withRouter(({user, push}) => (
   <Container>
     <Left>Pain Diary</Left>
       {!!user ? (
         <Right>
-          <LogoutButton onClick={() => auth.signOut()}><i className="fa fa-sign-out"></i> Logout</LogoutButton>
+          <Button onClick={() => auth.signOut()}><i className="fa fa-sign-out"></i> Logout</Button>
         </Right>
       ) : (
         <Right>
-          <Link to="/login">Login</Link>
+          <StyledLink to="/login"><i className="fa fa-sign-in"></i> Log In</StyledLink>
         </Right>
       )}
   </Container>
-)
+))
 
 Header.propTypes = {
   user: React.PropTypes.object
@@ -39,8 +39,23 @@ const Left = styled.div`
 const Right = styled.div`
 `
 
-const LogoutButton = styled.button`
-  background: #42d934;
+const Button = styled.button`
+  background-image: linear-gradient(to bottom, #42d934, #53914c);
+  border-radius: 8px;
+  font-family: Arial;
+  color: #ffffff;
+  font-size: 12px;
+  padding: 2px 5px 2px 5px;
+  text-decoration: none;
+
+  &:hover {
+    background: #3cb0fd;
+    background-image: linear-gradient(to bottom, #49e837, #65b35b);
+    text-decoration: none;
+  }
+`
+
+const StyledLink = styled(Link)`
   background-image: linear-gradient(to bottom, #42d934, #53914c);
   border-radius: 8px;
   font-family: Arial;
